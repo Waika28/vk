67eed6fd56d7c769bc1df13f60f23e28cc30bc36
Evgeny Rybin <z.ribin20@gmail.com>
2023-01-13 17:44:39 +0500

    Restore documentation in methods

--- lib/vk/methods/loader.ex
+++ lib/vk/methods/loader.ex
@@ -1,5 +1,8 @@
 defmodule VK.Methods.Loader do
   @moduledoc false
+
+  @vk_link_regex ~r/\[(.+?)\|(.+?)\]/
+
   defmacro __using__(path: path) do
     File.ls!(path)
     |> Stream.map(&Path.join([path, &1, "methods.json"]))
@@ -20,6 +23,7 @@ defmodule VK.Methods.Loader do
 
     quote do
       @spec unquote(fun_name)(Keyword.t()) :: {:ok, map()} | {:error, any()}
+      @doc unquote(generate_documentation(method))
       def unquote(fun_name)(params) when is_list(params) do
         VK.API.method_request(unquote(method_name), params)
       end
@@ -31,5 +35,61 @@ defmodule VK.Methods.Loader do
     |> String.replace(".", "_")
     |> Macro.underscore()
     |> String.to_atom()
+  end
+
+  defp generate_documentation(method) do
+    description =
+      if method["description"] do
+        format_vk_text(method["description"])
+      else
+        "description not provided"
+      end
+
+    """
+    #{description}
+
+
+    #{generate_documentation_for_parameters(method)}
+    """
+  end
+
+  defp generate_documentation_for_parameters(method) do
+    parameters = method["parameters"]
+
+    if parameters do
+      parameters =
+        parameters
+        |> Enum.map_join("\n\n", &format_param/1)
+
+      """
+      Parameters:
+
+      #{parameters}
+      """
+    end
+  end
+
+  defp format_param(param) do
+    name = param["name"]
+    description = param["description"]
+
+    if description do
+      description = format_vk_text(description)
+      "`#{name}` - #{description}"
+    else
+      "`#{name}`"
+    end
+  end
+
+  defp format_vk_text(text) do
+    text
+    |> format_vk_link_as_markdown()
+  end
+
+  defp format_vk_link_as_markdown(text) do
+    Regex.replace(@vk_link_regex, text, fn link_text ->
+      [_all, link, text] = Regex.run(@vk_link_regex, link_text)
+      "[#{text}](https://#{link})"
+    end)
   end
 end
